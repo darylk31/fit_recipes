887f4f487342731820c9f81bc9871e2de528acee
Daryl <darylkwok333@Gmail.com>
2017-12-07 01:31:01 -0800

    region update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_protocol => 'http',
+  :region => ENV['AWS_REGION'],
   :bucket => ENV['AWS_BUCKET'],
   :s3_credentials => {
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
